72d70a92aaa5256f6935f2c723444bece72e969e
Djamel Ziane <djamel.ziane@gmx.net>
2025-09-16 18:51:18 +0000

    add .gitignore

--- test.sql
+++ test.sql
@@ -2,5 +2,9 @@ CREATE TABLE [dbo].[Product](
 	[ProductID] [int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
 	[Name] [nvarchar](100) NOT NULL ,
 	deltaLine int ,
-	newnewval nvarchar(100)
+	newnewval nvarchar(100) ,
+	newvscol nvarchar(20)
 )
+
+
+execute sp_configure;
